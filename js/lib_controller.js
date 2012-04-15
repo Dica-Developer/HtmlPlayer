6049e0402bb89baeba9585f4d363b0a7fa0efd72
Martin Schaaf <mascha@ma-scha.de>
2012-04-15 14:03:26 +0200

    o issue #2 add backend and keyboard shortcuts for handling play/pause, previous and next switch

--- js/lib_controller.js
+++ js/lib_controller.js
@@ -1,40 +1,3 @@
-function collectAlbums(event) {
-  var req = event.target;
-  var ssr = req.responseXML.getElementsByTagName("subsonic-response");
-  if (null !== ssr && undefined !== ssr && ssr.length > 0 && "ok" === ssr[0].getAttribute("status")) {
-    var container = document.getElementById("albumsBox");
-    var albums = req.responseXML.getElementsByTagName("child");
-    for (var i = 0; i < albums.length; i++) {
-      var option = document.createElement("option");
-      var optionText = document.createTextNode(albums[i].getAttribute("title") +" ("+ albums[i].getAttribute("id") +")");
-      option.appendChild(optionText);
-      container.appendChild(option);
-
-      var image = document.getElementById('coverArt');
-      image.src = "https://streaming.one.ubuntu.com/rest/getCoverArt.view?u=" + JSON.parse(localStorage["authentication.login"]) + "&p=" +JSON.parse(localStorage["authentication.password"])+ "&v=1.2.0&c=chrome&id=" +albums[i].getAttribute("coverArt");
-    }
-  } else {
-    error = "fetching albums failed with status '" +ssr.getAttribute("status")+ "'";
-  }
-}
-
-function collectArtists(event) {
-	var req = event.target;
-  var ssr = req.responseXML.getElementsByTagName("subsonic-response");
-  if (null !== ssr && undefined !== ssr && ssr.length > 0 && "ok" === ssr[0].getAttribute("status")) {
-    var container = document.getElementById("artistsBox");
-    var artists = req.responseXML.getElementsByTagName("artist");
-    for (var i = 0; i < artists.length; i++) {
-      var option = document.createElement("option");
-      var optionText = document.createTextNode(artists[i].getAttribute("name") +" ("+ artists[i].getAttribute("id") +")");
-      option.appendChild(optionText);
-      container.appendChild(option);
-    }
-  } else {
-    error = "fetching artists failed with status '" +ssr.getAttribute("status")+ "'";
-  }
-}
-
 function collectSongs(event) {
   var req = event.target;
   var ssr = req.responseXML.getElementsByTagName("subsonic-response");
@@ -99,6 +62,20 @@ function getFirstPlaylistElement() {
     return JSON.parse(unescape(elements[0].value));
   } else {
     return null;
+  }
+}
+
+function setFirstPlaylistElement(song) {
+  var option = document.createElement("option");
+  option.value = escape(JSON.stringify(song));
+  var optionText = document.createTextNode(song.artist + " / " + song.album + " / " + song.track + ". " + song.title);
+  option.appendChild(optionText);
+  var container = document.getElementById("playlistBox");
+  var elements = $("#playlistBox :first");
+  if (elements.length > 0) {
+    container.insertBefore(option, elements[0]);
+  } else {
+    container.appendChild(option)
   }
 }
 
