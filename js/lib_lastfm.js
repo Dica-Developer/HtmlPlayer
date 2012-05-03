3979f3619b523f7922ef6d9430eb28598f67ab4c
Martin Schaaf <mascha@ma-scha.de>
2012-05-03 13:55:56 +0200

    o formatting: add sapce between function and curly braces

--- js/lib_lastfm.js
+++ js/lib_lastfm.js
@@ -1,4 +1,4 @@
-var Scrobbler = function(sessionKey, login){
+var Scrobbler = function(sessionKey, login) {
   this._serviceUrl = "http://ws.audioscrobbler.com/2.0/";
   this._apiKey = "ac2f676e5b95231ac4706b3dcb5d379d";
   this._secret = "29d73236629ddab3d9688d5378756134";
@@ -15,21 +15,21 @@ Scrobbler.prototype.getSession = function(token, successCB, errorCB) {
   $.ajax(this._serviceUrl + "?format=json&method=auth.getSession&api_key="+this._apiKey+"&api_sig="+signature+"&token="+token, {type: "GET", success: successCB, error: errorCB});
 }
 
-Scrobbler.prototype.setNowPlaying = function(artist, track, album, duration, successCB, errorCB){
+Scrobbler.prototype.setNowPlaying = function(artist, track, album, duration, successCB, errorCB) {
   if (this.isAuthenticated) {
     var signature = hex_md5("album" + album + "api_key" + this._apiKey + "artist" + artist + "duration" + duration + "methodtrack.updateNowPlayingsk" + this._sessionKey + "track" + track + this._secret);
     $.ajax(this._serviceUrl + "?format=json&method=track.updateNowPlaying&api_key=" + this._apiKey + "&api_sig=" + signature + "&sk=" + this._sessionKey + "&artist=" + artist + "&track=" + track + "&album=" + album + "&duration=" + duration, {type: "POST", success: successCB, error: errorCB});
   }
 }
 
-Scrobbler.prototype.scrobble = function(artist, track, album, duration, playStartTime, successCB, errorCB){
+Scrobbler.prototype.scrobble = function(artist, track, album, duration, playStartTime, successCB, errorCB) {
   if (this.isAuthenticated) {
     var signature = hex_md5("album" + album + "api_key" + this._apiKey + "artist" + artist + "duration" + duration + "methodtrack.scrobblesk" + this._sessionKey + "timestamp" + playStartTime + "track" + track + this._secret);
       $.ajax(this._serviceUrl + "?format=json&method=track.scrobble&api_key=" + this._apiKey + "&api_sig=" + signature + "&sk=" + this._sessionKey + "&artist=" + artist + "&track=" + track + "&album=" + album + "&duration=" + duration + "&timestamp=" + playStartTime, {type: "POST", success: successCB, error: errorCB});
   }
 }
 
-Scrobbler.prototype.isAuthenticated = function(){
+Scrobbler.prototype.isAuthenticated = function() {
   return null !== this_sessionKey && null !== this._login && undefined !== this_sessionKey && undefined !== this._login;
 }
 
