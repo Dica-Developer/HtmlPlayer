df9f9f4258a0695751c469a6b774169180fd0ee7
Martin Schaaf <mschaaf@datameer.com>
2012-07-31 21:04:29 +0200

    * add changes for manifest version 2

---
+++ js/lib_main.js
@@ -0,0 +1,426 @@
+var songHistory = new Array();
+var viewState = 'player';
+var notScrobbled = true;
+
+function addToHistory(song) {
+  songHistory.push(song)
+  if (songHistory.length > 1000) {
+    songHistory.shift();
+  }
+}
+
+function applyCoverArtStyle() {
+  document.getElementById("coverArt").height = $(document).height() * 0.6;
+  $("#coverArt").reflect({
+    height : 0.165,
+    opacity : 0.25
+  });
+}
+
+function next() {
+  var song = getFirstPlaylistElement()
+  if (null !== song) {
+    startPlay(song);
+    removeFirstPlaylistElement();
+    addToHistory(song);
+    applyCoverArtStyle();
+  }
+}
+
+function previous() {
+  if (songHistory.length > 0) {
+    var song = songHistory.pop();
+    if (null !== song) {
+      startPlay(song);
+      setFirstPlaylistElement(song);
+      applyCoverArtStyle();
+    }
+  }
+}
+
+function getLastSong() {
+  var song = null;
+  if (songHistory.length > 0) {
+    song = songHistory[songHistory.length - 1];
+  }
+  return song;
+}
+
+function updateProgress() {
+  var audio = document.getElementById('player');
+  if (!audio.paused) {
+    var progress = document.getElementById('progressBar');
+    progress.value = Math.round((audio.currentTime * 100) / audio.duration);
+  }
+}
+
+function setNowPlaying() {
+  var song = getLastSong();
+  if (null !== song) {
+    (new Scrobbler(localStorage["audica.lastfm.sessionKey"], localStorage["audica.lastfm.login"])).setNowPlaying(song.artist, song.title, song.album, song.duration, function(data, textStatus, jqXHR) {
+      if (undefined !== data.error) {
+        switch (data.error) {
+          case 6:
+          case 13:
+            console.warn("Cannot set now playing there is a parameter missing/wrong!", data.message);
+            break;
+          default:
+            alert("Cannot set last.fm now playing track. " + data.error + " - " + data.message);
+        }
+      }
+    }, null);
+  }
+}
+
+function updateTimings() {
+  if ($("#playerControlView").data("open")) {
+    var audio = document.getElementById('player');
+    if (!audio.paused) {
+      var field = document.getElementById('timeField');
+      field.innerHTML = Math.round(audio.currentTime) + " / " + Math.round(audio.duration);
+    }
+  }
+}
+
+function scrobble() {
+  var audio = document.getElementById('player');
+  if (!audio.paused) {
+    if (Math.round((audio.currentTime * 100) / audio.duration) > 50 && notScrobbled) {
+      var song = getLastSong();
+      if (null !== song) {
+        var timestamp = parseInt((new Date()).getTime() / 1000.0);
+        (new Scrobbler(localStorage["audica.lastfm.sessionKey"], localStorage["audica.lastfm.login"])).scrobble(song.artist, song.title, song.album, song.duration, timestamp, function(data, textStatus, jqXHR) {
+          if (undefined !== data.error) {
+            switch (data.error) {
+              case 6:
+              case 13:
+                console.warn("Cannot scrobble the song there is a parameter missing/wrong!", data.message);
+                notScrobbled = true;
+                break;
+              default:
+                alert("Cannot scrobble track to last.fm. " + data.error + " - " + data.message);
+            }
+          } else {
+            notScrobbled = false;
+          }
+        }, null);
+      }
+    }
+  }
+}
+
+function backgroundTasks() {
+  updateProgress();
+  updateTimings();
+  scrobble();
+}
+
+var closePlayerControlViewTimerId = null;
+
+function closePlayerControlView() {
+  $("#playerControlView").data("open", false);
+  closePlayerControlViewTimerId = null;
+  $("#playerControlView").animate({
+    height : "4px"
+  });
+}
+
+function updateSongList() {
+  var currentSongList = localStorage["songs.list"];
+  if (null !== currentSongList && undefined !== currentSongList) {
+    fillSongBox(JSON.parse(currentSongList), null);
+  }
+  searchForSongs("", collectSongs, null, null);
+}
+
+$(function() {
+  if (null === localStorage["serverUrl"] || undefined === localStorage["serverUrl"]) {
+    document.location = chrome.extension.getURL("options/index.html");
+  }
+  updateSongList();
+  $("#searchView").css("left", -1 * $(document).width());
+  $("#coverArtBox").css("padding-top", ($(document).height() - $("#coverArtBox").height()) / 2);
+  $("#descriptionBox").css("padding-top", ($(document).height() - $("#descriptionBox").height()) / 2);
+
+  $("#songBox").on("keyup", function(event) {
+    if (39 === event.which) {
+      $("#songBox :selected").clone().appendTo("#playlistBox");
+    }
+  });
+  $("#playlistBox").on("keyup", function(event) {
+    if (37 === event.which) {
+      $("#playlistBox :selected").detach();
+    }
+  });
+
+  function handleRightZone(event) {
+    if ('search' === viewState) {
+      if ("mouseenter" === event.type) {
+        $("#searchView").height($(document).height());
+        $("#searchView").animate({
+          left : -1 * $(document).width() * 0.05
+        });
+        $("#playerView").animate({
+          left : $(document).width() * 0.95
+        });
+        $("#playerControlView").animate({
+          left : $(document).width() * 0.95
+        });
+      } else if ("mouseleave" === event.type) {
+        $("#searchView").height($(document).height());
+        $("#searchView").animate({
+          left : "0"
+        });
+        $("#playerView").animate({
+          left : $(document).width()
+        });
+        $("#playerControlView").animate({
+          left : $(document).width()
+        });
+      } else if ("click" === event.type) {
+        $("#searchView").animate({
+          width : -1 * $(document).width()
+        });
+        $("#playerView").animate({
+          left : "0"
+        });
+        $("#playerControlView").animate({
+          left : "0"
+        });
+        var audio = document.getElementById('player');
+        if (audio.paused) {
+          next();
+          setNowPlaying();
+          notScrobbled = true;
+        }
+        viewState = 'player';
+        $("#coverArtBox").css("padding-top", ($(document).height() - $("#coverArtBox").height()) / 2);
+        $("#descriptionBox").css("padding-top", ($(document).height() - $("#descriptionBox").height()) / 2);
+      }
+    }
+  }
+
+
+  $("#playerViewPreview").on({
+    hover : handleRightZone,
+    click : handleRightZone
+  });
+
+  function handleLeftZone(event) {
+    if ('player' === viewState) {
+      if ("mouseenter" === event.type) {
+        $("#searchView").height($(document).height());
+        $("#searchView").animate({
+          left : -1 * $(document).width() * 0.95
+        });
+        $("#playerView").animate({
+          left : $(document).width() * 0.05
+        });
+        $("#playerControlView").animate({
+          left : $(document).width() * 0.05
+        });
+      } else if ("mouseleave" === event.type) {
+        $("#searchView").height($(document).height());
+        $("#searchView").animate({
+          left : -1 * $(document).width()
+        });
+        $("#playerView").animate({
+          left : "0"
+        });
+        $("#playerControlView").animate({
+          left : "0"
+        });
+      } else if ("click" === event.type) {
+        $("#songBox").focus();
+        var boxWidth = ($(document).width() / 2) - 20 - 2;
+        var boxHeight = $(document).height() - 22;
+        $("#songBox").width(boxWidth);
+        $("#songBox").height(boxHeight);
+        $("#playlistBox").width(boxWidth);
+        $("#playlistBox").height(boxHeight);
+        $("#searchView").height($(document).height());
+        $("#searchView").animate({
+          left : "0"
+        });
+        $("#playerView").animate({
+          left : $(document).width()
+        });
+        $("#playerControlView").animate({
+          left : $(document).width()
+        });
+        viewState = 'search';
+      }
+    }
+  }
+
+
+  $("#searchViewPreview").on({
+    hover : handleLeftZone,
+    click : handleLeftZone
+  });
+
+  $(document).mousemove(function(event) {
+    if ('player' === viewState) {
+      if (null !== closePlayerControlViewTimerId) {
+        clearTimeout(closePlayerControlViewTimerId);
+      }
+      if (!$("#playerControlView").data("open")) {
+        $("#playerControlView").data("open", true);
+        $("#playerControlView").animate({
+          height : "50px"
+        });
+      }
+      closePlayerControlViewTimerId = setTimeout("closePlayerControlView()", 3000);
+    }
+  });
+
+  var filterBoxTimeout = null;
+  $(document).on("keyup", function(event) {
+    if ('player' === viewState) {
+      switch (event.which) {
+        case 39:
+          var audio = document.getElementById('player');
+          audio.currentTime = audio.currentTime + 10;
+          break;
+        case 37:
+          var audio = document.getElementById('player');
+          audio.currentTime = audio.currentTime - 10;
+          break;
+        case 187:
+          var audio = document.getElementById('player');
+          audio.playbackRate = audio.playbackRate + 0.05;
+          break;
+        case 189:
+          var audio = document.getElementById('player');
+          audio.playbackRate = audio.playbackRate - 0.05;
+          break;
+        case 76:
+          $("#songBox").focus();
+          var boxWidth = ($(document).width() / 2) - 20 - 2;
+          var boxHeight = $(document).height() - 22;
+          $("#songBox").width(boxWidth);
+          $("#songBox").height(boxHeight);
+          $("#playlistBox").width(boxWidth);
+          $("#playlistBox").height(boxHeight);
+          $("#searchView").height($(document).height());
+          $("#searchView").animate({
+            left : "0"
+          });
+          $("#playerView").animate({
+            left : $(document).width()
+          });
+          $("#playerControlView").animate({
+            left : $(document).width()
+          });
+          viewState = 'search';
+          break;
+        case 80:
+          previous();
+          setNowPlaying();
+          notScrobbled = true;
+          break;
+        case 78:
+          next();
+          setNowPlaying();
+          notScrobbled = true;
+          break;
+        case 32:
+          var audio = document.getElementById('player');
+          if (audio.paused) {
+            audio.play();
+          } else {
+            audio.pause();
+          }
+          break;
+      }
+    } else if ('search' === viewState) {
+      switch (event.which) {
+        case 27:
+          if ($("#filterBox").data("open")) {
+            $("#filterBox").data("open", false);
+            $("#songBox").focus();
+            $("#filterBox").hide();
+            $('#filterBox').val("");
+          } else {
+            $("#searchView").animate({
+              left : -1 * $(document).width()
+            });
+            $("#playerView").animate({
+              left : "0"
+            });
+            $("#playerControlView").animate({
+              left : "0"
+            });
+            var audio = document.getElementById('player');
+            if (audio.paused) {
+              next();
+              setNowPlaying();
+              notScrobbled = true;
+            }
+            viewState = 'player';
+            $("#coverArtBox").css("padding-top", ($(document).height() - $("#coverArtBox").height()) / 2);
+            $("#descriptionBox").css("padding-top", ($(document).height() - $("#descriptionBox").height()) / 2);
+          }
+          break;
+        case 37:
+        case 38:
+        case 39:
+        case 40:
+        case 9:
+        case 16:
+        case 18:
+        case 17:
+          break;
+        default:
+          if (!$("#filterBox").data("open")) {
+            $("#filterBox").data("open", true);
+            // todo the first key should be filled in the filterBox
+            // but with keyup we only get a normal key and not chars that are created with two keys like !
+            // this works only with keypress
+            // $('#filterBox').val(String.fromCharCode(event.which));
+            $("#filterBox").show();
+          }
+          $('#filterBox').focus();
+          if (null !== filterBoxTimeout) {
+            clearTimeout(filterBoxTimeout);
+          }
+          filterBoxTimeout = setTimeout(function() {
+            var songList = JSON.parse(localStorage["songs.list"]);
+            fillSongBox(songList, $('#filterBox').val());
+          }, 500);
+          break;
+      }
+    } else {
+      console.log("Unknown view state '" + viewState + "'.");
+    }
+  });
+
+  $("#player").on("ended", function(event) {
+    next();
+    setNowPlaying();
+    notScrobbled = true;
+  });
+
+  $("#player").on("error", function(event) {
+    var audio = document.getElementById('player');
+    var errorMsg = "The file '" + audio.src + "' cannot be played. The possible reasons is: ";
+    switch (event.currentTarget.error.code) {
+      case 4:
+        errorMsg += "The current media type '" + audio.type + "' isn't supported.";
+        break;
+      case 1:
+        errorMsg += "The user agent stopped fetching the media data.";
+        break;
+      case 2:
+        errorMsg += "A network error stopped the user agent fetching the media data.";
+        break;
+      case 3:
+        errorMsg += "Error on decoding the media data.";
+        break;
+      default:
+        errorMsg += "Unknown error with code '" + event.currentTarget.error.code + "' happened."
+    }
+    alert(errorMsg);
+  });
+  setInterval("backgroundTasks()", 1000);
+});
--- options/main.js
+++ options/main.js
@@ -1,62 +1,62 @@
-function saveLogin(){
+function saveLogin() {
   var login = document.querySelector("#loginBox").value;
-	localStorage["authentication.login"] = JSON.stringify(login);
+  localStorage["authentication.login"] = JSON.stringify(login);
 }
 
-function savePassword(){
+function savePassword() {
   var password = document.querySelector("#passwordBox").value;
-	localStorage["authentication.password"] = JSON.stringify(password);
+  localStorage["authentication.password"] = JSON.stringify(password);
 }
 
-function saveServerUrl(){
+function saveServerUrl() {
   var serverUrl = document.querySelector("#serverUrlBox").value;
-	localStorage["serverUrl"] = JSON.stringify(serverUrl);
+  localStorage["serverUrl"] = JSON.stringify(serverUrl);
 }
 
-function fill(){
-	var password = localStorage["authentication.password"];
-	if (null !== password && undefined !== password) {
-	  document.querySelector("#passwordBox").value = JSON.parse(password);
-	}
-	var login = localStorage["authentication.login"];
-	if (null !== login && undefined !== login) {
-	  document.querySelector("#loginBox").value = JSON.parse(login);
-	}
-	var serverUrl = localStorage["serverUrl"];
-	if (null !== serverUrl && undefined !== serverUrl) {
-	  var serverUrlClear = JSON.parse(serverUrl);
-	  document.querySelector("#serverUrlBox").value = serverUrlClear;
-	  if ("https://streaming.one.ubuntu.com" === serverUrlClear) {
-	    selectOption($("#backendSelection"), "ubuntuone");
-	  } else {
-	    selectOption($("#backendSelection"), "subsonic");
-	  }
-	  initBackend(null);
-	}
-	var lastFmLogin = localStorage["audica.lastfm.login"];
-	if (null !== lastFmLogin && undefined !== lastFmLogin) {
-	  document.querySelector("#lastfmUserLink").innerHTML = lastFmLogin;
-	  document.querySelector("#lastfmUserLink").style.display = "block";
-	  document.querySelector("#lastfmUserLabel").style.display = "block";
-	  document.querySelector("#lastfmLogoutLink").style.display = "block";
-	  document.querySelector("#lastfmLoginLink").style.display = "none";
-	} else {
-	  document.querySelector("#lastfmLoginLink").style.display = "block";
+function fill() {
+  var password = localStorage["authentication.password"];
+  if (null !== password && undefined !== password) {
+    document.querySelector("#passwordBox").value = JSON.parse(password);
+  }
+  var login = localStorage["authentication.login"];
+  if (null !== login && undefined !== login) {
+    document.querySelector("#loginBox").value = JSON.parse(login);
+  }
+  var serverUrl = localStorage["serverUrl"];
+  if (null !== serverUrl && undefined !== serverUrl) {
+    var serverUrlClear = JSON.parse(serverUrl);
+    document.querySelector("#serverUrlBox").value = serverUrlClear;
+    if ("https://streaming.one.ubuntu.com" === serverUrlClear) {
+      selectOption($("#backendSelection"), "ubuntuone");
+    } else {
+      selectOption($("#backendSelection"), "subsonic");
+    }
+    initBackend(null);
+  }
+  var lastFmLogin = localStorage["audica.lastfm.login"];
+  if (null !== lastFmLogin && undefined !== lastFmLogin) {
+    document.querySelector("#lastfmUserLink").innerHTML = lastFmLogin;
+    document.querySelector("#lastfmUserLink").style.display = "block";
+    document.querySelector("#lastfmUserLabel").style.display = "block";
+    document.querySelector("#lastfmLogoutLink").style.display = "block";
+    document.querySelector("#lastfmLoginLink").style.display = "none";
+  } else {
+    document.querySelector("#lastfmLoginLink").style.display = "block";
     document.querySelector("#lastfmLogoutLink").style.display = "none";
     document.querySelector("#lastfmUserLink").style.display = "none";
     document.querySelector("#lastfmUserLabel").style.display = "none";
-	}
-	document.querySelector("#backend").onclick = selectTab;
-	document.querySelector("#scrobble").onclick = selectTab;
-	document.querySelector("#about").onclick = selectTab;
+  }
+  document.querySelector("#backend").onclick = selectTab;
+  document.querySelector("#scrobble").onclick = selectTab;
+  document.querySelector("#about").onclick = selectTab;
 }
 
 function selectTab() {
   var currentTab = document.querySelector("li.navbar-item-selected")
   currentTab.classList.remove("navbar-item-selected");
-  document.querySelector("#"+ currentTab.id +"Page").style.display = "none";
+  document.querySelector("#" + currentTab.id + "Page").style.display = "none";
   this.classList.add("navbar-item-selected");
-  document.querySelector("#"+ this.id +"Page").style.display = "block";
+  document.querySelector("#" + this.id + "Page").style.display = "block";
 }
 
 function initBackend(event) {
@@ -83,3 +83,54 @@ function logoutFromLastFm() {
   delete localStorage["audica.lastfm.login"];
 }
 
+function lastFmLoginClick(e) {
+  e.target.href = (new Scrobbler(null, null)).getTokenUrl();
+}
+
+function lastFmLoginClick(e) {
+  e.target.href = 'http://last.fm/user/' + e.target.innerHTML;
+}
+
+$(function() {
+  fill();
+
+  document.querySelector('#lastfmLoginLink').addEventListener('click', lastFmLoginClick);
+  document.querySelector('#lastfmLogoutLink').addEventListener('click', logoutFromLastFm);
+  document.querySelector('#lastfmUserLink').addEventListener('click', logoutFromLastFm);
+  document.querySelector('#backendSelection').addEventListener('change', initBackend);
+  document.querySelector('#serverUrlBox').addEventListener('change', saveServerUrl);
+  document.querySelector('#loginBox').addEventListener('change', saveLogin);
+  document.querySelector('#passwordBox').addEventListener('change', savePassword);
+
+  chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
+    var pattLastFM = new RegExp("^chrome-extension://.+/options/authenticate_lastfm\.html.token=(.+)$");
+    var patt = new RegExp("^ubuntuone://(.+):(.+)@syncml\.one\.ubuntu\.com$");
+    if (patt.test(request.url)) {
+      var match = patt.exec(request.url);
+      document.querySelector("#loginBox").value = match[1];
+      document.querySelector("#passwordBox").value = match[2];
+      document.querySelector("#serverUrlBox").value = "https://streaming.one.ubuntu.com";
+      saveLogin();
+      savePassword();
+      saveServerUrl();
+    } else if (pattLastFM.test(request.url)) {
+      var match = pattLastFM.exec(request.url);
+      (new Scrobbler(null, null)).getSession(match[1], function(data, textStatus, jqXHR) {
+        if (undefined === data.error) {
+          localStorage["audica.lastfm.sessionKey"] = data.session.key;
+          localStorage["audica.lastfm.login"] = data.session.name;
+          document.querySelector("#lastfmUserLink").innerHTML = data.session.name;
+          document.querySelector("#lastfmUserLink").style.display = "block";
+          document.querySelector("#lastfmLogoutLink").style.display = "block";
+          document.querySelector("#lastfmLoginLink").style.display = "none";
+          document.querySelector("#lastfmUserLabel").style.display = "block";
+        } else {
+          alert("It failed to get a session key." + data.error + " - " + data.message);
+        }
+      }, null);
+    } else {
+      alert("It failed to retreive the authentication credentials. The returned url is invalid '" + request.url + "'.");
+    }
+    chrome.tabs.remove(sender.tab.id);
+  });
+})
