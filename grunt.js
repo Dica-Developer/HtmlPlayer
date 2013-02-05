e7a172c1e27bd2a872404efa5bcbbc5f6d08e8bb
Jörg Weber <weber_joerg.h@web.de>
2013-02-06 00:21:22 +0100

    * add grunt job to run jasmine tests

--- grunt.js
+++ grunt.js
@@ -1,14 +1,28 @@
 /*global module:true, require:true, process:true*/
-module.exports = function(grunt) {
+module.exports = function (grunt) {
   "use strict";
 
   // Project configuration.
   grunt.initConfig({
-    lint: {
-      all: ['Gruntfile.js', 'js/**/*.js', 'options/**/*.js']
+    jasmine_node:{
+      specNameMatcher:"./test/example/spec/*.js",
+      projectRoot:".",
+      requirejs:false,
+      forceExit:true,
+      jUnit:{
+        report:false,
+        savePath:"./build/reports/jasmine/",
+        useDotNotation:true,
+        consolidate:true
+      }
+    },
+    lint:{
+      all:['Gruntfile.js', 'js/**/*.js', 'options/**/*.js']
     }
   });
 
-  grunt.registerTask('test', ['lint']);
+
+  grunt.loadNpmTasks('grunt-jasmine-node');
+  grunt.registerTask('test', ['lint', 'jasmine_node']);
 
 };
