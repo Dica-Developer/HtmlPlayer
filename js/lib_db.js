54b2844455d2f8f316d25d05ef28cec3ab28b56b
Jörg Weber <weber_joerg.h@web.de>
2013-02-06 11:35:45 +0100

    * refactor lib_db

--- js/lib_db.js
+++ js/lib_db.js
@@ -1,19 +1,23 @@
-function Db(){
-  var _dbName = null;
+/*global window, TAFFY, localStorage*/
+(function (window) {
+  "use strict";
+  window.Db = function () {
+    var _dbName = null;
 
-  this.query = null;
+    this.query = null;
 
-  this.init = function(dbName) {
-    _dbName = "db." + dbName;
-    var dbContent = localStorage[_dbName];
-    if (null !== dbContent && undefined !== dbContent) {
-      this.query = TAFFY(JSON.parse(dbContent));
-    } else {
-      this.query = TAFFY();
-    }
+    this.init = function (dbName) {
+      _dbName = "db." + dbName;
+      var dbContent = localStorage[_dbName];
+      if (null !== dbContent && undefined !== dbContent) {
+        this.query = TAFFY(JSON.parse(dbContent));
+      } else {
+        this.query = TAFFY();
+      }
+    };
+
+    this.save = function () {
+      localStorage[_dbName] = JSON.stringify(this.query().get());
+    };
   };
-
-  this.save = function() {
-    localStorage[_dbName] = JSON.stringify(this.query().get());
-  }
-}
+})(window);
