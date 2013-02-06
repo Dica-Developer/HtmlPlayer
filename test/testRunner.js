b7beb1b8276d4c91dfc39dd16075dfaf6e3d4a53
Martin Schaaf <mschaaf@datameer.com>
2013-02-06 08:59:15 +0100

    * add test run summary * exit phantom with 0 in case of an error * fix js lint stuff

--- test/testRunner.js
+++ test/testRunner.js
@@ -1,44 +1,61 @@
+"use strict";
+
+/*global phantom, $, console, require, setInterval, clearInterval*/
+
 var page = require('webpage').create();
 
 function waitFor(testFx, onReady, timeOutMillis) {
-    var maxtimeOutMillis = timeOutMillis ? timeOutMillis : 3000,
-        //< Default Max Timout is 3s
-        start = new Date().getTime(),
-        condition = false,
-        interval = setInterval(function () {
-            if ((new Date().getTime() - start < maxtimeOutMillis) && !condition) {
-                // If not time-out yet and condition not yet fulfilled
-                condition = (typeof (testFx) === "string" ? eval(testFx) : testFx());
-            } else {
-                if (!condition) {
-                    // If condition still not fulfilled (timeout but condition is 'false')
-                    console.log("'waitFor()' timeout");
-                    phantom.exit(1);
-                } else {
-                    // Condition fulfilled (timeout and/or condition is 'true')
-                    console.log("'waitFor()' finished in " + (new Date().getTime() - start) + "ms.");
-                    typeof (onReady) === "string" ? eval(onReady) : onReady();
-                    clearInterval(interval); //< Stop this interval
-                }
-            }
-        }, 250);
+  var maxtimeOutMillis = timeOutMillis || 3000;
+  var start = new Date().getTime(),
+    condition = false,
+    interval = setInterval(function () {
+      if ((new Date().getTime() - start < maxtimeOutMillis) && !condition) {
+        condition = testFx();
+      } else {
+        if (!condition) {
+          console.log("'waitFor()' timeout");
+          phantom.exit(1);
+        } else {
+          console.log("'waitFor()' finished in " + (new Date().getTime() - start) + "ms.");
+          onReady();
+          clearInterval(interval);
+        }
+      }
+    }, 250);
 }
 
 page.open('./test/example/SpecRunner.html', function (status) {
-    page.viewportSize = {
-        width: 1280,
-        height: 800
-    };
-    if (status !== 'success') {
-        console.log('Unable to access the network!');
-    } else {
-        waitFor(function () {
-            return page.evaluate(function () {
-                return $("span.finished-at").is(":visible");
-            });
-        }, function () {
-            page.render('result.png');
-            phantom.exit();
-        }, 30000);
-    }
+  page.viewportSize = {
+    width: 1280,
+    height: 800
+  };
+  if (status !== 'success') {
+    console.log('Unable to access the network!');
+  } else {
+    waitFor(function () {
+      return page.evaluate(function () {
+        return $("span.finished-at").is(":visible");
+      });
+    }, function () {
+      var result = page.evaluate(function () {
+        var msg = '';
+        var passed = $('.suite .passed');
+        var failed = $('.suite .failed');
+        msg = msg + passed.length + ' passed tests of ' + (passed.length + failed.length) + '\n';
+        msg = msg + failed.length + ' failed tests of ' + (passed.length + failed.length) + '\n';
+        msg = msg + $("span.finished-at").text();
+        var error = failed.length > 0 ? true : false;
+        return {
+          'msg': msg,
+          'error': error
+        };
+      });
+      console.log(result.msg);
+      if (result.error) {
+        phantom.exit(1);
+      } else {
+        phantom.exit(0);
+      }
+    }, 30000);
+  }
 });
