269bff979beba576a918d584676124f44709457d
Jörg Weber <weber_joerg.h@web.de>
2013-02-06 10:17:26 +0100

    * move all 3rd party js in an extra folder

--- js/3rd/oauth2/oauth2.js
+++ js/3rd/oauth2/oauth2.js
@@ -348,7 +348,7 @@ OAuth2.loadAdapter = function(adapterName, callback) {
   var head = document.querySelector('head');
   var script = document.createElement('script');
   script.type = 'text/javascript';
-  script.src = '/js/oauth2/adapters/' + adapterName + '.js';
+  script.src = '/js/3rd/oauth2/adapters/' + adapterName + '.js';
   script.addEventListener('load', function() {
     callback();
   });
