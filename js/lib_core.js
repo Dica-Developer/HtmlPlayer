8747d77807eb18288af45ac3971b93040120a489
Jörg Weber <weber_joerg.h@web.de>
2013-02-27 22:05:27 +0100

    * try to ensure all dom elements are set, otherwise throw error

--- js/lib_core.js
+++ js/lib_core.js
@@ -382,13 +382,29 @@
   };
 
   Audica.prototype.initDom = function() {
+    var self = this;
     for (var selector in this.Dom) {
       if (this.Dom.hasOwnProperty(selector)) {
         this.Dom[selector] = $('#' + selector);
       }
     }
-    this.trigger('domElementsSet');
-    this.applyCoverArtStyle();
+
+    function checkDomElements(){
+      for (var selector in self.Dom) {
+        if (self.Dom.hasOwnProperty(selector)) {
+          if(null === self.Dom[selector]){
+            self.Dom[selector] = $('#' + selector);
+            if(null === self.Dom[selector]){
+              throw new Error(selector + 'not set!');
+            }
+          }
+        }
+      }
+      self.trigger('domElementsSet');
+      self.applyCoverArtStyle();
+    }
+
+    checkDomElements();
   };
 
   Audica.prototype.setFirstPlaylistElement = function(song) {
