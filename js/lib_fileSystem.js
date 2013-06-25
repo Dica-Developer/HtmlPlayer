2be4bbf52209be44eee5dfc1fed1a5b117d64cfc
Martin Schaaf <mschaaf@datameer.com>
2013-06-25 23:11:48 +0200

    * format * trigger init ready on all possible places

--- js/lib_fileSystem.js
+++ js/lib_fileSystem.js
@@ -1,6 +1,7 @@
 /*global $, FileError, console, Audica, PERSISTENT, window*/
 (function (window) {
   "use strict";
+
   function Filesystem() {
     var backendId = 'filesystem';
     var fileSystem = null;
@@ -9,28 +10,30 @@
      *
      * @param {Event} e
      */
+
     function errorHandler(e) {
       var msg = '';
       switch (e.code) {
-        case FileError.QUOTA_EXCEEDED_ERR:
-          msg = 'QUOTA_EXCEEDED_ERR';
-          break;
-        case FileError.NOT_FOUND_ERR:
-          msg = 'NOT_FOUND_ERR';
-          break;
-        case FileError.SECURITY_ERR:
-          msg = 'SECURITY_ERR';
-          break;
-        case FileError.INVALID_MODIFICATION_ERR:
-          msg = 'INVALID_MODIFICATION_ERR';
-          break;
-        case FileError.INVALID_STATE_ERR:
-          msg = 'INVALID_STATE_ERR';
-          break;
-        default:
-          msg = 'Unknown Error: ' + e;
-          break;
+      case FileError.QUOTA_EXCEEDED_ERR:
+        msg = 'QUOTA_EXCEEDED_ERR';
+        break;
+      case FileError.NOT_FOUND_ERR:
+        msg = 'NOT_FOUND_ERR';
+        break;
+      case FileError.SECURITY_ERR:
+        msg = 'SECURITY_ERR';
+        break;
+      case FileError.INVALID_MODIFICATION_ERR:
+        msg = 'INVALID_MODIFICATION_ERR';
+        break;
+      case FileError.INVALID_STATE_ERR:
+        msg = 'INVALID_STATE_ERR';
+        break;
+      default:
+        msg = 'Unknown Error: ' + e;
+        break;
       }
+      Audica.trigger('initReady');
       throw msg;
     }
 
@@ -39,7 +42,7 @@
      * @param {Number} timestamp
      * @private
      */
-    var _searchForSongs = function(timestamp) {
+    var _searchForSongs = function (timestamp) {
       var reader = fileSystem.root.createReader();
       reader.readEntries(function (results) {
         var songList = [];
@@ -47,7 +50,11 @@
           var song = readFile(results[i], timestamp);
           songList.push(song);
         }
-        Audica.trigger('readyCollectingSongs', {songList:songList, backendId:backendId, timestamp:timestamp});
+        Audica.trigger('readyCollectingSongs', {
+          songList: songList,
+          backendId: backendId,
+          timestamp: timestamp
+        });
       }, function (e) {
         console.error(e);
       });
@@ -59,24 +66,25 @@
      * @param {Number} timestamp
      * @return {Object|Null}
      */
+
     function readFile(entry, timestamp) {
       var song = null;
       if (entry.isFile) {
         song = {
-          "artist":'Unknown',
-          "album":'Unknown',
-          "title":entry.name,
-          "id":entry.name,
-          "coverArt":'',
-          "contentType":entry.type,
-          "track":0,
-          "cd":0,
-          "duration":0,
-          "genre":'',
-          "year":1900,
-          "addedOn":timestamp,
-          "src":entry.toURL(),
-          "backendId":backendId
+          "artist": 'Unknown',
+          "album": 'Unknown',
+          "title": entry.name,
+          "id": entry.name,
+          "coverArt": '',
+          "contentType": entry.type,
+          "track": 0,
+          "cd": 0,
+          "duration": 0,
+          "genre": '',
+          "year": 1900,
+          "addedOn": timestamp,
+          "src": entry.toURL(),
+          "backendId": backendId
         };
       } else {
         console.log('Cannot handle "' + entry.name + '". It is a file.');
@@ -88,13 +96,13 @@
       player.src = src;
     };
 
-    this.setCoverArt = function () {
-    };
+    this.setCoverArt = function () {};
 
     /**
      *
      * @param fs
      */
+
     function onInitFs(fs) {
       fileSystem = fs;
       Audica.on('updateSongList', function (args) {
@@ -108,13 +116,14 @@
     }
 
     this.init = function () {
-      if(window.webkitStorageInfo){
+      if (window.webkitStorageInfo) {
         window.webkitStorageInfo.requestQuota(PERSISTENT, 1024 * 1024 * 1024, function (grantedBytes) {
           window.webkitRequestFileSystem(PERSISTENT, grantedBytes, onInitFs, errorHandler);
         }, function (e) {
           console.error('Error: ' + e);
+          Audica.trigger('initReady');
         });
-      }else{
+      } else {
         console.error('No webkitStorage available');
         Audica.trigger('initReady');
       }
