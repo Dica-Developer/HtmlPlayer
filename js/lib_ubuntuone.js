302795768729efc2d5d185a3855b83cf04f40525
Martin Schaaf <mschaaf@datameer.com>
2013-06-25 23:05:52 +0200

    * add init method * handle not setupped plugin

--- js/lib_ubuntuone.js
+++ js/lib_ubuntuone.js
@@ -1,27 +1,77 @@
-/*global Audica:true, localStorage: true, XMLHttpRequest:true, console:true*/
+/*global Audica:true, localStorage: true, XMLHttpRequest:true, console:true, window*/
 (function (window, Audica) {
   "use strict";
 
   /**
    * @class
    */
+
   function Subsonic() {
     /**
      * @type {String}
      */
     var backendId = 'subsonic';
+
     /**
      * @type {String}
      * @private
      */
-    var _login = JSON.parse(localStorage.authentication_login);
+    var _login = null;
+
     /**
      * @type {String}
      * @private
      */
-    var _password = JSON.parse(localStorage.authentication_password);
+    var _password = null;
 
     var _streamingUrl = 'https://streaming.one.ubuntu.com/rest';
+
+    var _serverUrl = null;
+
+    /**
+     * @param {Event} event
+     * @param {Number} timestamp
+     * @private
+     */
+
+    function _collect(event, timestamp) {
+      var req = event.target;
+      var ssr = req.responseXML.getElementsByTagName("subsonic-response");
+      if (null !== ssr && undefined !== ssr && ssr.length > 0 && "ok" === ssr[0].getAttribute("status")) {
+        var songs = req.responseXML.getElementsByTagName("match");
+        var songList = [],
+          i = 0,
+          length = songs.length,
+          song;
+        for (i; i < length; i++) {
+          song = songs[i];
+          var songObj = {
+            "artist": song.getAttribute("artist"),
+            "album": song.getAttribute("album"),
+            "title": song.getAttribute("title"),
+            "id": song.getAttribute("id"),
+            "coverArt": song.getAttribute('coverArt') ? _serverUrl + '/getCoverArt.view?u=' + _login + '&p=' + _password + "&v=1.2.0&c=chrome&id=" + song.getAttribute('coverArt') : null,
+            "contentType": song.getAttribute("contentType"),
+            "track": song.getAttribute("track") ? parseInt(song.getAttribute("track"), 0) : null,
+            "cd": 0,
+            "duration": song.getAttribute("duration"),
+            "genre": song.getAttribute("genre"),
+            "year": song.getAttribute("year") ? parseInt(song.getAttribute("year"), 0) : null,
+            "addedOn": timestamp,
+            "src": _streamingUrl + '/stream.view?u=' + _login + '&p=' + _password + '&v=1.2.0&c=chrome&id=' + song.getAttribute("id"),
+            "backendId": backendId
+          };
+          songList.push(songObj);
+        }
+        Audica.trigger('readyCollectingSongs', {
+          songList: songList,
+          backendId: backendId,
+          timestamp: timestamp
+        });
+      } else {
+        console.error("fetching songs failed with status '" + ssr.getAttribute("status") + "'");
+      }
+    }
 
     /**
      * @param {Number} timestamp
@@ -29,52 +79,20 @@
      * @param {Function} collectProgress
      * @private
      */
-    function _searchForSongs(timestamp, collectErrors, collectProgress) { //TODO collectErrors, collectProgress
-      var url = JSON.parse(localStorage.serverUrl) + "/search.view?u=" + JSON.parse(localStorage.authentication_login) + "&p=" + JSON.parse(localStorage.authentication_password) + "&v=1.2.0&c=chrome&count=100000&any=";
-      var req = new XMLHttpRequest();
-      req.open("GET", url, true);
-      req.onload = function (event) {
-        _collect(event, timestamp);
-      };
-      req.onerror = collectErrors;
-      req.onprogress = collectProgress;
-      req.send(null);
-    }
 
-    /**
-     * @param {Event} event
-     * @param {Number} timestamp
-     * @private
-     */
-    function _collect(event, timestamp) {
-      var req = event.target;
-      var ssr = req.responseXML.getElementsByTagName("subsonic-response");
-      if (null !== ssr && undefined !== ssr && ssr.length > 0 && "ok" === ssr[0].getAttribute("status")) {
-        var songs = req.responseXML.getElementsByTagName("match");
-        var songList = [], i = 0, length = songs.length, song;
-        for (i; i < length; i++) {
-          song = songs[i];
-          var songObj = {
-            "artist":song.getAttribute("artist"),
-            "album":song.getAttribute("album"),
-            "title":song.getAttribute("title"),
-            "id":song.getAttribute("id"),
-            "coverArt":song.getAttribute('coverArt') ? JSON.parse(localStorage.serverUrl) + '/getCoverArt.view?u=' + _login + '&p=' + _password + "&v=1.2.0&c=chrome&id=" + song.getAttribute('coverArt') : null,
-            "contentType":song.getAttribute("contentType"),
-            "track":song.getAttribute("track") ? parseInt(song.getAttribute("track"), 0) : null,
-            "cd":0,
-            "duration":song.getAttribute("duration"),
-            "genre":song.getAttribute("genre"),
-            "year":song.getAttribute("year") ? parseInt(song.getAttribute("year"), 0) : null,
-            "addedOn":timestamp,
-            "src":_streamingUrl + '/stream.view?u=' + _login + '&p=' + _password + '&v=1.2.0&c=chrome&id=' + song.getAttribute("id"),
-            "backendId":backendId
-          };
-          songList.push(songObj);
-        }
-        Audica.trigger('readyCollectingSongs', {songList:songList, backendId:backendId, timestamp:timestamp});
+    function _searchForSongs(timestamp, collectErrors, collectProgress) { //TODO collectErrors, collectProgress
+      if (_serverUrl && _login && _password) {
+        var url = _serverUrl + "/search.view?u=" + _login + "&p=" + _password + "&v=1.2.0&c=chrome&count=100000&any=";
+        var req = new XMLHttpRequest();
+        req.open("GET", url, true);
+        req.onload = function (event) {
+          _collect(event, timestamp);
+        };
+        req.onerror = collectErrors;
+        req.onprogress = collectProgress;
+        req.send(null);
       } else {
-        console.error("fetching songs failed with status '" + ssr.getAttribute("status") + "'");
+        // log error
       }
     }
 
@@ -89,8 +107,23 @@
     Audica.on('updateSongList', function (args) {
       _searchForSongs(args.timestamp, null, null);
     });
+
+    this.init = function () {
+      var login = localStorage.authentication_login;
+      var password = localStorage.authentication_password;
+      var serverUrl = localStorage.serverUrl;
+      if (login) {
+        _login = JSON.parse(login);
+      }
+      if (password) {
+        _password = JSON.parse(password);
+      }
+      if (serverUrl) {
+        _serverUrl = JSON.parse(serverUrl);
+      }
+      Audica.trigger('initReady');
+    };
   }
 
   Audica.extend('subsonic', new Subsonic());
-})(window, Audica);
-
+}(window, Audica));
