74e70e0702bdbc25ee96f7863cc9d44678e8471e
Martin Schaaf <mschaaf@datameer.com>
2014-06-22 14:51:19 +0200

    * add chrome launcher, seems also important because of chrome app and android platform * format

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,42 +1,41 @@
-module.exports = function (config) {
-    'use strict';
+/*global module*/
+module.exports = function(config) {
+  'use strict';
 
-    config.set({
-        basePath: '..',
-        frameworks: ['jasmine'],
+  config.set({
+    basePath: '..',
+    frameworks: ['jasmine'],
 
-        files: [
-            {
-                pattern: 'js/3rd/*.js',
-                included: true
-            },
-            'js/lib_core.js',
-            'js/lib_keys.js',
-            'js/lib_player.js',
-            {
-                pattern: 'test/lib/jasmine-jquery.js',
-                included: true
-            },
-            {
-                pattern: 'test/fixtures/*.*',
-                included: false
-            },
-            {
-                pattern: 'test/spec/Helper.js',
-                included: true
-            },
-            {
-                pattern: 'test/spec/*Spec.js',
-                included: true
-            }
-        ],
+    files: [{
+        pattern: 'js/3rd/*.js',
+        included: true
+      },
+      'js/lib_core.js',
+      'js/lib_keys.js',
+      'js/lib_player.js', {
+        pattern: 'test/lib/jasmine-jquery.js',
+        included: true
+      }, {
+        pattern: 'test/fixtures/*.*',
+        included: false
+      }, {
+        pattern: 'test/spec/Helper.js',
+        included: true
+      }, {
+        pattern: 'test/spec/*Spec.js',
+        included: true
+      }
+    ],
 
-        browsers: ['Firefox'],
-        reporters: ['dots'],
+    browsers: [
+      'Firefox',
+      'Chrome'
+    ],
+    reporters: ['dots'],
 
-        logLevel: config.LOG_INFO,
+    logLevel: config.LOG_INFO,
 
-        autoWatch: false,
-        singleRun: true
-    });
+    autoWatch: false,
+    singleRun: true
+  });
 };
