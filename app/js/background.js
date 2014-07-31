0f775e32915743458d7e02954e8026e07035c5ae
Jörg Weber <weber.joerg.h@gmail.com>
2014-07-31 21:04:19 +0200

    * code formatting

--- app/js/background.js
+++ app/js/background.js
@@ -1,7 +1,9 @@
 /*global chrome*/
 chrome.app.runtime.onLaunched.addListener(function () {
-  chrome.app.window.create('main.html', {
-    'state': 'maximized'
-  }, function () {
-  });
+    'use strict';
+    
+    chrome.app.window.create('main.html', {
+        'state': 'maximized'
+    }, function () {
+    });
 });
