a44ac0d35a182186b693e595678cc35bd73e401b
Jörg Weber <weber.joerg.h@gmail.com>
2014-07-31 09:09:01 +0200

    * make jshint happy

--- app/js/plugins/lib_fileImporter.js
+++ app/js/plugins/lib_fileImporter.js
@@ -1,75 +1,75 @@
-/*global $, PERSISTENT, Audica, FileError, console*/
+/*global $, PERSISTENT, Audica, FileError*/
 (function (window, Audica) {
-  "use strict";
+    'use strict';
 
-  function FileImporter() {
-    var idx = 0;
-    var fileSystem = null;
+    function FileImporter() {
+        var idx = 0;
+        var fileSystem = null;
 
-    function errorHandler(e) {
-      var msg = '';
-      switch (e.code) {
-        case FileError.QUOTA_EXCEEDED_ERR:
-          msg = 'QUOTA_EXCEEDED_ERR';
-          break;
-        case FileError.NOT_FOUND_ERR:
-          msg = 'NOT_FOUND_ERR';
-          break;
-        case FileError.SECURITY_ERR:
-          msg = 'SECURITY_ERR';
-          break;
-        case FileError.INVALID_MODIFICATION_ERR:
-          msg = 'INVALID_MODIFICATION_ERR';
-          break;
-        case FileError.INVALID_STATE_ERR:
-          msg = 'INVALID_STATE_ERR';
-          break;
-        default:
-          msg = 'Unknown Error: ' + e;
-          break;
-      }
-      throw msg;
+        function errorHandler(e) {
+            var msg = '';
+            switch (e.code) {
+            case FileError.QUOTA_EXCEEDED_ERR:
+                msg = 'QUOTA_EXCEEDED_ERR';
+                break;
+            case FileError.NOT_FOUND_ERR:
+                msg = 'NOT_FOUND_ERR';
+                break;
+            case FileError.SECURITY_ERR:
+                msg = 'SECURITY_ERR';
+                break;
+            case FileError.INVALID_MODIFICATION_ERR:
+                msg = 'INVALID_MODIFICATION_ERR';
+                break;
+            case FileError.INVALID_STATE_ERR:
+                msg = 'INVALID_STATE_ERR';
+                break;
+            default:
+                msg = 'Unknown Error: ' + e;
+                break;
+            }
+            throw msg;
+        }
+
+        function onInitFs(fs) {
+            fileSystem = fs;
+            Audica.trigger('initReady');
+        }
+
+
+        function writeFile(file) {
+            // TODO reset index per timestamp and check for file name exists
+            var fileName = 'fileImporter_' + $.now() + '_' + (++idx);
+            fileSystem.root.getFile(fileName, {create: true}, function (fileEntry) {
+                fileEntry.createWriter(function (fileWriter) {
+                    fileWriter.write(file);
+                }, errorHandler);
+            }, errorHandler);
+        }
+
+        this.writeFiles = function (files) {
+            for (var i = 0; i < files.length; i++) {
+                var file = files[i];
+                writeFile(file);
+            }
+            if (Audica) {
+                Audica.trigger('filesImported');
+            }
+        };
+
+        this.init = function () {
+            if (window.webkitStorageInfo) {
+                window.webkitStorageInfo.requestQuota(PERSISTENT, 1024 * 1024 * 1024, function (grantedBytes) {
+                    window.webkitRequestFileSystem(PERSISTENT, grantedBytes, onInitFs, errorHandler);
+                }, function (e) {
+                    Audica.trigger('ERROR', e);
+                });
+            } else {
+                Audica.trigger('WARN', 'No webkitStorage available');
+                Audica.trigger('initReady');
+            }
+        };
     }
 
-    function onInitFs(fs) {
-      fileSystem = fs;
-      Audica.trigger('initReady');
-    }
-
-
-    function writeFile(file) {
-      // TODO reset index per timestamp and check for file name exists
-      var fileName = 'fileImporter_' + $.now() + '_' + (++idx);
-      fileSystem.root.getFile(fileName, {create:true}, function (fileEntry) {
-        fileEntry.createWriter(function (fileWriter) {
-          fileWriter.write(file);
-        }, errorHandler);
-      }, errorHandler);
-    }
-
-    this.writeFiles = function (files) {
-      for (var i = 0; i < files.length; i++) {
-        var file = files[i];
-        writeFile(file);
-      }
-      if (Audica) {
-        Audica.trigger('filesImported');
-      }
-    };
-
-    this.init = function () {
-      if(window.webkitStorageInfo){
-        window.webkitStorageInfo.requestQuota(PERSISTENT, 1024 * 1024 * 1024, function (grantedBytes) {
-          window.webkitRequestFileSystem(PERSISTENT, grantedBytes, onInitFs, errorHandler);
-        }, function (e) {
-          console.error('Error: ' + e);
-        });
-      }else{
-        console.error('No webkitStorage available');
-        Audica.trigger('initReady');
-      }
-    };
-  }
-
-  Audica.extend('fileImporter', new FileImporter());
+    Audica.extend('fileImporter', new FileImporter());
 })(window, Audica);
